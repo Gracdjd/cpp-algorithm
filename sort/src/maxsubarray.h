79e7c84be7fe2d2ffaf7c9d594b316c84f121210
wz <2072401390@qq.com>
2023-09-17 16:15:18 +0800

    通过了寻找最大子串

--- sort/src/maxsubarray.h
+++ sort/src/maxsubarray.h
@@ -45,6 +45,51 @@ namespace Algorithm
         return std::make_pair(left_max, right_max);
 
     }
+    //求字数族的和
+    template<typename Iterator>
+    typename std::iterator_traits<Iterator>::value_type sumOfSubarray(Iterator begin, Iterator end)
+    {
+        typename std::iterator_traits<Iterator>::value_type sum = *begin;
+        while((++begin) != end)
+        {
+            sum += *begin;
+        }
+        return sum;
+    }
+
+
+    //主函数
+    template<typename Iterator, typename Compare = std::greater<typename std::iterator_traits<Iterator>::value_type>,
+    typename compareGE = std::greater_equal<typename std::iterator_traits<Iterator>::value_type>>
+    std::pair<Iterator, Iterator> findMaxSubarray(Iterator begin, Iterator end, Compare compare = Compare(), compareGE ge = compareGE())
+    {
+        if(begin == end) return std::make_pair(begin, end);
+        else if(std::distance(begin, end) == 1) return std::make_pair(begin, end);
+        else
+        {
+            Iterator mid = begin + std::distance(begin, end)/2;
+            std::pair<Iterator, Iterator> leftMax = findMaxSubarray(begin, mid, compare);
+            std::pair<Iterator, Iterator> rightMax = findMaxSubarray(mid, end, compare);
+
+            std::pair<Iterator, Iterator> crossMidMax = find_max_crossing_subarray(begin, mid, end, compare);
+
+
+            if(ge(sumOfSubarray(leftMax.first, leftMax.second), sumOfSubarray(rightMax.first, rightMax.second)) &&
+                ge(sumOfSubarray(leftMax.first, leftMax.second), sumOfSubarray(crossMidMax.first, crossMidMax.second)))
+            {
+                return leftMax;
+            }
+            else if(ge(sumOfSubarray(rightMax.first, rightMax.second), sumOfSubarray(leftMax.first, leftMax.second)) &&
+                ge(sumOfSubarray(rightMax.first, rightMax.second), sumOfSubarray(crossMidMax.first, crossMidMax.second)))
+            {
+                return rightMax;
+            }
+            else
+            {
+                return crossMidMax;
+            }
+        }
+    }
         
     } // namespace Sort
     
--- sort/test/maxsubarray.h
+++ sort/test/maxsubarray.h
@@ -5,11 +5,17 @@
 #include "../src/maxsubarray.h"
 #include <vector>
 using Algorithm::Sort::find_max_crossing_subarray;
-
+using Algorithm::Sort::findMaxSubarray;
 #define FIND_SUB(input, compare) begin = input.begin(); \
                         end = input.end(); \
                         mid = begin + std::distance(begin, end)/2; \
                         auto compare = find_max_crossing_subarray(begin,mid, end)
+
+
+#define FIND_MAX(input, compare) begin = input.begin(); \
+                        end = input.end(); \
+                        auto compare = findMaxSubarray(begin,end)
+
 
 TEST(test_sort_subarray, find_max_crossing_subarray)
 {
@@ -23,6 +29,22 @@ TEST(test_sort_subarray, find_max_crossing_subarray)
     std::vector<int> input2{2, 3, 4, 5, -1, -5};
     std::pair<std::vector<int>::iterator, std::vector<int>::iterator> data2{input2.begin(), input2.end() - 3};
     FIND_SUB(input2, compare2);
+    EXPECT_EQ(compare1, data1);
+    EXPECT_EQ(compare2, data2);
+}
+
+TEST(test_sort_subarray, findMaxSubarray)
+{
+    std::vector<int> input1={1,2,3,4,5,6,7,8,9,10};
+    std::pair<std::vector<int>::iterator, std::vector<int>::iterator> data1{input1.begin(), input1.end() - 1}; 
+    auto begin = input1.begin();
+    auto end = input1.end();
+    auto mid = begin + std::distance(begin, end) / 2;
+    auto  compare1 = findMaxSubarray(begin, end);
+
+    std::vector<int> input2{2, 3, 4, 5, -1, -5};
+    std::pair<std::vector<int>::iterator, std::vector<int>::iterator> data2{input2.begin(), input2.end() - 3};
+    FIND_MAX(input2, compare2);
     EXPECT_EQ(compare1, data1);
     EXPECT_EQ(compare2, data2);
 }
