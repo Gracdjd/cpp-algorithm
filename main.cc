b1f07991341315fc613afbc074aa22b63d2fd136
wz <2072401390@qq.com>
2023-09-18 09:39:01 +0800

    filter

--- main.cc
+++ main.cc
@@ -1,8 +1,10 @@
 #include "sort/test/insertsort_test.h"
-#include "sort/test/maxsubarray.h"
+#include "sort/test/maxsubarray_test.h"
+#include "sort/test/mergesort_test.h"
 int main(int argc,char** argv)
 {
     ::testing::InitGoogleTest(&argc,argv);
+    // ::testing::FLAGS_gtest_filter = "test_mergesort.test_std_cointer";
     return RUN_ALL_TESTS();
 }
 
